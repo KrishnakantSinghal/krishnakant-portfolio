2216452c5f0d7d569aca63156f44f4546f09b962
Krishna Kant Singhal <krishnakant.singhal@mail.codetrade.io>
2024-01-19 17:34:34 +0530

    bug fixing

--- krishnakant_portfolio/src/App.js
+++ krishnakant_portfolio/src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
+import "./App.css";
 import React, { useState, useEffect } from "react";
-import NavBar from './components/Navbar';
+import NavBar from "./components/Navbar";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  Navigate
+  Navigate,
 } from "react-router-dom";
-import ScrollToTop
-from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import Preloader from "../src/components/Pre";
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import Projects from "./components/Projects/Projects";
@@ -30,23 +29,22 @@ function App() {
 
   return (
     <section>
-      
-    <Router>
-      <Preloader load={load}/>
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <NavBar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </Router>
+      <Router basename={process.env.REACT_APP_PUBLIC_URL}>
+        <Preloader load={load} />
+        <div className="App" id={load ? "no-scroll" : "scroll"}>
+          <NavBar />
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      </Router>
     </section>
   );
 }
